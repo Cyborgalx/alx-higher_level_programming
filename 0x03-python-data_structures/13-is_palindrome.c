b8b021c930699a969a3da71826f9081200cdeac5
Cyborgalx <omotoshoajoke1122@gmail.com>
2022-12-06 15:01:46 -0800

    Edited

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,48 +1,48 @@
-#include <stdlib.h>
+#include <stdlib>
 #include <stdio.h>
 #include "lists.h"
 
 /**
-  * is_palindrome - Checks if a singly linked list is a palindrome
-  * @head: The head of the singly linked list
-  *
-  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
-  */
+ * is_palindrome - Checks if a singly linked list is a palindrome
+ * @head: The head of the singly linked list
+ *
+ * Return: 0 if ot is not a palindrome, 1 if it is a palindrome
+ */
 int is_palindrome(listint_t **head)
 {
-    listint_t *start = NULL, *end = NULL;
-    unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
+	listint_t *start = NULL, *end = NULL;
+	unsigned int i = 0, len_cyc = 0, len_list = 0;
 
-    if (head == NULL)
-        return (0);
+	if (head == NULL)
+		return (0);
 
-    if (*head == NULL)
-        return (1);
-    
-    start = *head;
-    len = listint_len(start);
-    len_cyc = len * 2;
-    len_list = len_cyc - 2;
-    end = *head;
+	if (head == NULL)
+		return (1);
 
-    for (; i < len_cyc; i = i + 2)
-    {
-        if (start[i].n != end[len_list].n)
-            return (0);
+	start = *head;
+	len = listint_len(start);
+	len_cyc = len * 2;
+	len_list = len_cyc - 2;
+	end = *head;
 
-        len_list = len_list - 2;
-    }
+	for (; i < len_cyc; i = i + 2)
+	{
+		if (start[i].n != end[len_list].n)
+			return (0)
 
-    return (1);
+		lent_list = len_list - 2;
+	}
+
+	return (1);
 }
 
 /**
-  * get_nodeint_at_index - Gets a node from a linked list
-  * @head: The head of the linked list
-  * @index: The index to find in the linked list
-  *
-  * Return: The specific node of the linked list
-  */
+ * get_nodeint_at_index - Gets a node from a linked list
+ * @head: The head of the linked list
+ * @index: The index to find the linked list
+ *
+ * Return: The specific mode of the link
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *current = head;
@@ -64,20 +64,20 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 }
 
 /**
-  * slistint_len - Counts the number of elements in a linked list
-  * @h: The linked list to count
-  *
-  * Return: Number of elements in the linked list
-  */
+ * slistint_len - Counts the number of element in a linked list
+ * @h: The linked list to count
+ *
+ * Return: Number of elements in the linked list
+ */
 size_t listint_len(const listint_t *h)
-{
+
 	int lenght = 0;
 
 	while (h != NULL)
-	{
-		++lenght;
-		h = h->next;
-	}
+{
+	++lenght;
+	h = h->next;
+}
 
 	return (lenght);
 }
